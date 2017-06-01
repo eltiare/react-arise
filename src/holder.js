bcd572d6ceecf9fe84c706986caf7194567c41a7
Jeremy Nicoll <jnicoll@accentuate.me>
2017-06-01 13:22:47 -0700

    Initial extract.

---
+++ src/holder.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class HolderComponent extends React.Component {
+
+  get passPropsToState() { return ['show']; }
+
+  constructor(props) {
+    super(props);
+    this._bindFunctions('open', 'close', '_reposition');
+    this.state = this._stateFromProps(props);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState( this._stateFromProps(newProps, this.props) );
+  }
+
+  componentDidMount() {
+    this._handleTransitions();
+    this._reposition();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this._handleTransitions(prevState);
+    this._reposition();
+  }
+
+  open() { this.setState({ show: true }); }
+
+  close() { this.setState({ show: false }); }
+
+  toggle() { this.setState({ show: !this.state.show }); }
+
+  render() {
+    const { show } = this.state;
+    const { html, children, modal, popupClass } = this.props;
+    const modalClasses = this.props.modalClasses || {};
+    const contentOpts = html ?
+      { dangerouslySetInnerHTML : { __html: html } } : { children };
+    if (modal) {
+      return <div className={ modalClasses.container || 'Holder-modal-container' } ref="container">
+        <div ref="overlay" className={ modalClasses.overlay || 'Holder-modal-overlay' }
+          onClick={ this.close } />
+        <div className={ modalClasses.content || 'Holder-modal-content '} ref="content"
+          { ... contentOpts } />
+      </div>;
+    } else {
+      return <div className={ popupClass || 'Holder-popup' } ref="container"
+        { ... contentOpts } />;
+    }
+  }
+
+  _handleTransitions(prevState = {}) {
+    if (prevState.show == this.state.show) return;
+    let { container } = this.refs;
+    let { showClass, hideTransitionClass } = this.props;
+    let sc = showClass || 'Holder-show',
+      htc = hideTransitionClass || 'Holder-hide-transition',
+      cl = container.classList;
+    const listener = (e) => {
+      container.removeEventListener('transitionend', listener);
+      cl.remove(htc);
+    };
+    if (this.state.show) {
+      cl.add(sc);
+    } else {
+      container.addEventListener('transitionend', listener);
+      cl.remove(sc);
+      cl.add(htc);
+    }
+  }
+
+  _reposition() {
+    let { modal, anchorElement, popupPadding, universalPositioning } = this.props;
+    let { show } = this.state;
+    let { container } = this.refs;
+    if (modal || !show) return;
+    let bottom, left;
+    if (universalPositioning) {
+      let bcr = anchorElement.getBoundingClientRect();
+      bottom = bcr.bottom + ( window.scrollY || window.pageYOffset );
+      left = bcr.left + ( window.scrollX || window.pageXOffset );
+    } else {
+      bottom = anchorElement.offsetTop + anchorElement.offsetHeight;
+      left = anchorElement.offsetLeft;
+    }
+    container.style.top = `calc(${ bottom }px + ${ popupPadding || '5px' })`;
+    container.style.left = left + 'px';
+  }
+
+  _bindFunctions() {
+    let arg;
+    for (let i=0; arg = arguments[i]; i++) {
+      this[arg] = this[arg].bind(this);
+    }
+  }
+
+  _stateFromProps(newProps, oldProps) {
+    let vars = this.passPropsToState, p;
+    let newState = {};
+    oldProps = oldProps || {};
+    for (let i=0; p = vars[i]; i++) {
+      if (newProps[p] !== oldProps[p]) newState[p] = newProps[p];
+    }
+    this.setState(newState);
+  }
+
+}
+
+let div;
+export default function holder(props) {
+  if (!div) {
+    div = document.createElement('div');
+    document.body.appendChild(div);
+  }
+  return ReactDOM.render( <HolderComponent { ... props } universalPositioning={ true } />, div );
+}
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+let path = require('path');
+let webpack = require('webpack');
+let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let nodeExternals = require('webpack-node-externals');
+
+let env = process.env.NODE_ENV || 'dev';
+let prod = env === 'production';
+
+let plugins = [
+  new ExtractTextPlugin("[name]"),
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'server',
+    analyzerHost: '127.0.0.1',
+    analyzerPort: 8889
+  })
+];
+
+let config = {
+  devtool: prod ? false : 'source-map',
+  entry: {
+    "index.js":    "./src/holder.js",
+    "holder.css":  "./src/holder.scss"
+  },
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: '[name]'
+  },
+  externals: prod ? [ nodeExternals() ] : [],
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [ 'es2015', { modules: false } ],
+            [ "env", {
+              "targets" : {
+                "browsers" : ["last 3 versions", "ie >= 9"]
+              }
+            }],
+            'react',
+            'stage-0'
+          ],
+          plugins: [
+            "transform-export-extensions",
+            "add-module-exports",
+            [ "module-resolver", {
+                "root": ["."],
+                "alias": {
+                  "react": "preact-compat",
+                  "react-dom": "preact-compat",
+                  "create-react-class": "preact-compat/lib/create-react-class"
+                }
+              }
+            ]
+          ]
+        },
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.(s)?css$/,
+        loader: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              loader: "css-loader!resolve-url-loader!sass-loader?sourceMap"
+            })
+      },
+    ]
+  }
+};
+
+module.exports = config;
